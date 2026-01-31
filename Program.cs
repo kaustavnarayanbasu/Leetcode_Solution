2c0765dc67d68acede386183ca3e4dc72b5debe4
Kaustav Narayan Basu <kaustavnarayanbasu@gmail.com>
2026-01-31 23:06:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Leetcode_Solution;
+
+Console.WriteLine("Enter characters (e.g., cfj):");
+string input = Console.ReadLine();   // user types cfj
+char[] letters = input.ToCharArray(); // convert string to char array
+
+Array.Sort(letters);
+foreach (char c in letters)
+{
+    Console.Write(c + " ");
+}
+
+Console.WriteLine("Enter terget character:");
+char target = Console.ReadLine()[0]; // user types a
+SmallestLetterGreterThanTerget solution = new SmallestLetterGreterThanTerget();
+char returnedChar = solution.NextGreatestLetter(letters, target);
+Console.WriteLine($"The smallest letter greater than '{target}' is: '{returnedChar}'");
---
+++ SmallestLetterGreterThanTerget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solution
+{
+    internal class SmallestLetterGreterThanTerget
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            foreach (char letter in letters)
+            {
+                if (letter > target)
+                {
+                    return letter;
+                }
+            }
+            // If no character is greater, wrap around and return the first
+            return letters[0];
+        }
+
+    }
+}
