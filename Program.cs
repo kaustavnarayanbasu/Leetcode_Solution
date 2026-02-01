255cebafc4d49faa095eb89b498efc3a8ce8d01b
Kaustav Narayan Basu <kaustavnarayanbasu@gmail.com>
2026-02-01 21:21:17 +0530

    Added Two Sums Code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Leetcode_Solution;
+Console.WriteLine("Enter numbers (e.g., 123):");
+string inputStr = Console.ReadLine();
+int[] input = inputStr?.Select(c => int.Parse(c.ToString())).ToArray() ?? Array.Empty<int>();
+Console.WriteLine("Enter target number:");
+int target = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter characters (e.g., cfj):");
-string input = Console.ReadLine();   // user types cfj
-char[] letters = input.ToCharArray(); // convert string to char array
-
-Array.Sort(letters);
-foreach (char c in letters)
+int[] result = TwoSum.FindTwoSum(input, target);
+if (result.Length > 0)
 {
-    Console.Write(c + " ");
+    Console.WriteLine($"Indices: {result[0]}, {result[1]}");
+    Console.WriteLine($"Values: {input[result[0]]}, {input[result[1]]}");
+}
+else
+{
+    Console.WriteLine("No two elements found with the given sum.");
 }
 
-Console.WriteLine("Enter terget character:");
-char target = Console.ReadLine()[0]; // user types a
-SmallestLetterGreterThanTerget solution = new SmallestLetterGreterThanTerget();
-char returnedChar = solution.NextGreatestLetter(letters, target);
-Console.WriteLine($"The smallest letter greater than '{target}' is: '{returnedChar}'");
+//char returnedChar = solution.NextGreatestLetter(letters, target);
+//Console.WriteLine($"The smallest letter greater than '{target}' is: '{returnedChar}'");
+
+//Console.WriteLine("Enter characters (e.g., cfj):");
+//string input = Console.ReadLine();   // user types cfj
+//char[] letters = input.ToCharArray(); // convert string to char array
+
+//Array.Sort(letters);
+//foreach (char c in letters)
+//{
+//    Console.Write(c + " ");
+//}
+
+//Console.WriteLine("Enter terget character:");
+//char target = Console.ReadLine()[0]; // user types a
+//SmallestLetterGreterThanTerget solution = new SmallestLetterGreterThanTerget();
+//char returnedChar = solution.NextGreatestLetter(letters, target);
+//Console.WriteLine($"The smallest letter greater than '{target}' is: '{returnedChar}'");
+
---
+++ TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Solution
+{
+    internal class TwoSum
+    {
+        public static int[] FindTwoSum(int[] nums, int target)
+        {
+
+            int sum = 0;
+
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j <= nums.Length - 1; j++)
+                {
+                    sum = nums[i] + nums[j];
+                    if (sum == target)
+                    {
+                        return new int[] { i, j };
+
+                    }
+                    
+                }
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
